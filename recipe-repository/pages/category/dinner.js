02857657492d012bb8900579b14a73c69e6ff166
marian10g <marianmacaso5@gmail.com>
2024-03-05 18:22:01 +0800

    Recipe Repository
    added dinner page

--- recipe-repository/pages/category/dinner.js
+++ recipe-repository/pages/category/dinner.js
@@ -6,11 +6,37 @@ function dinner(){
         <>
         <Header/>
         <div className="dinner-container">
-            <div>
+            <div className="dinner-title">
                 <h1>Dinner</h1>
                 <p>Make your evenings special with our selected dinner ideas, adding a gourmet touch to create a memorable nights.</p>
             </div>
+
+         <div className="dinner-recipe">
+
+                <div className= "first-recipe">
+
+                     <h1>Chicken Adobo</h1>
+                </div>
+
+                <div className= "second-recipe">
+                     <h1>Beef Nilaga</h1>
         </div>
+
+        <div className= "third-recipe">
+            <h1>Chicken Tinola</h1>
+        </div>
+
+        <div className= "fourth-recipe">
+            <h1>Bulalo</h1>
+        </div>
+
+        <div className= "fifth-recipe">
+            <h1>Sinigang</h1>
+        </div>
+        </div>   
+        
+        </div>
+        
         <Footer/>
         </>
     )
