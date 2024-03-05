21d0c9f6e1382a1b84647db3e5b171e7594523b8
trishamlpz <trishamlpz@gmail.com>
2024-03-05 19:04:07 +0800

    Recipe Reposotory
    - navbar
    -lunch

--- recipe-repository/components/Constants/Navbar.js
+++ recipe-repository/components/Constants/Navbar.js
@@ -12,8 +12,6 @@ function Navbar(){
                 <li className="mobile-hidden"><Link href="/category/allRecipes">ALL RECIPES</Link></li>
                 <li className="mobile-hidden"><Link href="#">BREAKFAST</Link></li>
                 <li className="mobile-hidden"><Link href="/category/lunch">LUNCH</Link></li>
-                <li className="mobile-hidden"><Link href="#">DINNER</Link></li>
-                <li className="mobile-hidden"><Link href="#">LUNCH</Link></li>
                 <li className="mobile-hidden"><Link href="/category/dinner">DINNER</Link></li>
             </ul>
         </nav>
