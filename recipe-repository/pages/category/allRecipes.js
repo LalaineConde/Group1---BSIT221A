8a5edeebe3adae732ee0d0d178e209dce938f467
LalaineConde <161687040+LalaineConde@users.noreply.github.com>
2024-03-06 03:05:07 +0800

    Recipe Repository
    created the content of all recipes

--- recipe-repository/pages/category/allRecipes.js
+++ recipe-repository/pages/category/allRecipes.js
@@ -1,15 +1,43 @@
-import Header from "/components/Constants/Header"
-import Footer from "/components/Constants/Footer"
+import React from 'react';
+import Header from "/components/Constants/Header";
+import Footer from "/components/Constants/Footer";
+import recipes from "./recipe.json";
 
-
-function allRecipes(){
-    return(
-        <>
-        <Header/>
-        <h1 className="title">All Recipes</h1>
-        <Footer/>
-        </>
-    );
+function RecipeContainer({ recipe, index }) {
+  return (
+    <div className={`recipe-container ${index % 2 === 1 ? 'even' : ''}`}>
+      <div className="left-column">
+        <img className="AllRecipes-image" src={recipe.image} alt={recipe.name} />
+      </div>
+      <div className="right-column">
+        <h2 className="recipe-name">{recipe.name}</h2>
+        <p className="recipe-description">{recipe.description}</p>
+        <button className="recipe-button">Get Recipe</button>
+      </div>
+    </div>
+  );
 }
 
-export default allRecipes
+function AllRecipes() {
+  return (
+    <>
+      <Header />
+
+    <main className='body'>
+    <h1 className="AllRecipes-title">All Recipes</h1>
+    <p className='AllRecipes-description'>Discover a variety of tasty dishes in our recipe collection that suits every taste and occasion.</p>
+
+      <div className='AllRecipes-container'> 
+      
+        {recipes.map((recipe, index) => (
+          <RecipeContainer key={index} recipe={recipe} index={index} />
+        ))}
+      </div>
+
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default AllRecipes;
