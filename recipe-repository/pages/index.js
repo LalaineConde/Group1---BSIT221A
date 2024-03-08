5c512f117dbe4a943984e0df5f2844b5de20cf52
JonRussellGA <garciajonrussell13@gmail.com>
2024-03-09 00:53:57 +0800

    Recipe Repository
    Modify code in index/home

--- recipe-repository/pages/index.js
+++ recipe-repository/pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
   const [dinnerRecipe, setDinnerRecipe] = useState(null);
   const router = useRouter();
 
-  const dailyMilliseconds = 24 * 60 * 60 * 1000; 
+  const dailyMilliseconds = 24 * 60 * 60; 
 
   useEffect(() => {
     const date = new Date();
