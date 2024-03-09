9a0d3b44d5cb031ac78180793e2862e632e143ae
marian10g <marianmacaso5@gmail.com>
2024-03-09 14:40:47 +0800

    Revert "Merge branch 'main' of https://github.com/LalaineConde/Group1---BSIT221A"
    This reverts commit 5abcbc2a7305c7feff3df88a4665b4a47c057fcc, reversing
    changes made to b0f111766be005e730bf253e606a4ffea0e54b47.

--- recipe-repository/components/Constants/Navbar.js
+++ recipe-repository/components/Constants/Navbar.js
@@ -1,33 +1,23 @@
+
 import Link from "next/link";
-import { useState } from "react";
+function Navbar(){
 
-function Navbar() {
-    const [showSidebar, setShowSidebar] = useState(false);
-
-    const toggleSidebar = () => {
-        setShowSidebar(!showSidebar);
-    };
-
-    return (
+    
+    return(
         <>
-            <nav className="navbar">
-                <div className="nav-container">
-                    <div className={`menuToggle ${showSidebar ? 'active' : ''}`} onClick={toggleSidebar}>
-                        <div className="bar"></div>
-                        <div className="bar"></div>
-                        <div className="bar"></div>
-                    </div>
-                    <ul className={`nav-list ${showSidebar ? 'active' : ''}`}>
-                        <li className="mobile-hidden"><Link href="/">HOME</Link></li>
-                        <li className="mobile-hidden"><Link href="/category/allRecipes">ALL RECIPES</Link></li>
-                        <li className="mobile-hidden"><Link href="/category/breakfast">BREAKFAST</Link></li>
-                        <li className="mobile-hidden"><Link href="/category/lunch">LUNCH</Link></li>
-                        <li className="mobile-hidden"><Link href="/category/dinner">DINNER</Link></li>
-                    </ul>
-                </div>
-            </nav>
+       
+        <nav className="navbar">
+            <ul className="nav-list">
+                <li className="mobile-hidden"><Link href="/">HOME</Link></li>
+                <li className="mobile-hidden"><Link href="/category/allRecipes">ALL RECIPES</Link></li>
+                <li className="mobile-hidden"><Link href="/category/breakfast">BREAKFAST</Link></li>
+                <li className="mobile-hidden"><Link href="/category/lunch">LUNCH</Link></li>
+                <li className="mobile-hidden"><Link href="/category/dinner">DINNER</Link></li>
+            </ul>
+        </nav>
+        
         </>
     );
 }
 
-export default Navbar;
+export default Navbar
