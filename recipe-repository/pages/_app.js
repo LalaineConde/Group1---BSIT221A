52eddf032cb3fd9c3ed9bb9c333690a851bed4b0
LalaineConde <161687040+LalaineConde@users.noreply.github.com>
2024-03-12 12:32:49 +0800

    Recipe Repository
    Website Title in home page

--- recipe-repository/pages/_app.js
+++ recipe-repository/pages/_app.js
@@ -4,11 +4,15 @@ import "/styles/recipe_category_layout.scss"
 import Header from "/components/Constants/Header"
 import Footer from "/components/Constants/Footer"
 import Searchbar from "/components/Constants/Searchbar.js"
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  const isHomePage = router.pathname === '/';
   return (
     <>
     <Header/>
+    {isHomePage && <div className="website-title-container"><h1 className='website-title'>FOODIE DELIGHT</h1></div>}
     <Searchbar/>
     <Component {...pageProps} />
     <Footer/>
